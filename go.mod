c57cfcb79d8c164a01f631d4f88ae390741048c4
dzivdzi <aleksandar_st@live.com>
2024-02-08 22:24:13 +0200

    first

---
+++ go.mod
@@ -0,0 +1,7 @@
+module mymodule
+
+go 1.21.6
+
+require github.com/dzivdzi/puppy v0.0.0-20240208200420-001f5df6ec19
+
+require github.com/dzivdzi/dog v0.0.0-20240208195846-ec87075a1536 // indirect
---
+++ mod-code-depend-2/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dzivdzi/puppy"
+)
+
+func main() {
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+
+	s3 := puppy.BigBark()
+	s4 := puppy.BigBarks()
+
+	fmt.Println(s3)
+	fmt.Println(s4)
+}
